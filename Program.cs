5ea87d1ce8b513105158e30359394708478466f3
brunorobertocataneo <bruno31.cataneo@gmail.com>
2025-10-29 20:44:12 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// Namespace definido como TimSort
+namespace TimSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Array inicial para teste
+            int[] array = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+
+            Console.WriteLine("Array antes da ordenação (TimSort):");
+            Console.WriteLine(string.Join(", ", array));
+
+            // Chamada para o método Sort da classe TimSort
+            TimSort.Sort(array);
+
+            Console.WriteLine("Array após a ordenação (TimSort):");
+            Console.WriteLine(string.Join(", ", array));
+
+            // Fim do programa principal
+        }
+    } // Fim da classe Program
+} // Fim do namespace TimSort
